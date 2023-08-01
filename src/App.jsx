99975674821e42bb1115348fd9642921c1234884
Blake Rossdeutcher <blakerossdeutcher@gmail.com>
2023-08-01 11:48:41 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom"
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div id="container"><h1>Hello</h1>
+        <div id="navbar">{/* navigation here */}</div>
+        <div id="main-section">
+          <Routes>
+            <Route path="/blue" element={<h1>Blue</h1>}></Route>
+            <Route path="/red"  element={<h1>Red</h1>}></Route>
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
